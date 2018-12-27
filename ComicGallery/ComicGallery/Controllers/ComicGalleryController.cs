d10894c37cb00ae7359e985178b6aa6cdf451668
Chris Troy <ctcoding0627@gmail.com>
2018-12-27 14:22:14 -0800

    Init Commit with Controller and View

---
+++ ComicGallery/ComicGallery/Controllers/ComicGalleryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicGallery.Controllers
+{
+    public class ComicGalleryController : Controller
+    {
+        // GET: ComicGallery
+        public ActionResult Detail()
+        {
+            ViewBag.SeriesTitle = "The Amazing SpiderMan";
+            ViewBag.IssueNum = 700;
+            ViewBag.Artists = new string[]
+            {
+                 "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' " +
+                        "life and his one, last, great act of revenge! Even if Spider-Man survives... " +
+                        "<strong>will Peter Parker?</strong></p>";
+
+            return View();
+        }
+    }
+}
