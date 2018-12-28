67b31fc4ff2e3e9747274188f9eaedf1adea7b10
Chris Troy <ctcoding0627@gmail.com>
2018-12-28 12:43:25 -0800

    Refactored Detail view to be strongly typed

--- ComicGallery/ComicGallery/Models/ComicBook.cs
+++ ComicGallery/ComicGallery/Models/ComicBook.cs
@@ -11,7 +11,15 @@ namespace ComicGallery.Models
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
-        public Artists[] Artitsts { get; set; }
+        public Artists[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+
+        //Remember leaving out the set makes this a readonly property
+
+        //this is the short hand for setting a get or readonly propety
+        public string DisplayText => SeriesTitle + " #" + IssueNumber;
+
+        public string CoverImageFileName => SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
     }
 }
